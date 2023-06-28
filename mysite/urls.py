2faed7ac65c5f43a599f2ec607a8b5f02361db94
jordjii <georgiy.alexandrov@bk.ru>
2023-06-28 20:46:44 +0300

    1

---
+++ mysite/urls.py
@@ -0,0 +1,18 @@
+
+
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.conf import settings
+from django.contrib.auth.views import LogoutView, LoginView
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('blog/', include(('blog.urls', 'blog'))),
+    path('logout/', LogoutView.as_view()),
+    path('login/', LoginView.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
